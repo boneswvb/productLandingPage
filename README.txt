ef76ef0038c19536aa0627cb1a53582a4f202f18
boneswvb@gmail.com <boneswvb@gmail.com>
2019-03-23 10:51:09 +0200

    loading all files first time

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/Bones1/pen/XxZJPw.
+
+ Free code camp project
+All code done self.
+Added some humor to the project.
---
+++ js/index.js
@@ -0,0 +1,20 @@
+var button = document.getElementById("bayButton");
+
+function bay() {
+  alert("Sorry - can't get it");
+}
+button.addEventListener("click", bay);
+
+var button1 = document.getElementById("bayButton1");
+
+function bay() {
+  alert("Sorry - can't get it");
+}
+button1.addEventListener("click", bay);
+
+var button2 = document.getElementById("bayButton2");
+
+function bay() {
+  alert("Sorry - can't get it");
+}
+button2.addEventListener("click", bay);
